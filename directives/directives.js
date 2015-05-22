352c8fcdc8904b8b6fa26ec7e6be856e36a9b3e1
Sumen Tse <sumen.tse@gmail.com>
2015-05-22 17:42:19 -0400

    Fix issue with gratuity input accepting signed floating numbers

--- directives/directives.js
+++ directives/directives.js
@@ -5,13 +5,15 @@ angular.module("myDirectives", [])
 	        restrict: 'A',
 	        require: 'ngModel',
 	        link: function (scope, element, attrs, ngModel) {
+	            
+
 	            if (!ngModel) return;
 	            ngModel.$parsers.unshift(function (inputValue) {
 	                var digits = inputValue.split('').filter(function (s) { return (!isNaN(s) && s != ' '); }).join('');
 	                
 	                //limits how many items should be brought
-	                if(digits > 99){
-	                  digits = 99;
+	                if(digits > attrs.max){
+	                  digits = attrs.max;
 	                }
 	                else if(digits == 0){
 	                  digits = 1;
@@ -22,5 +24,53 @@ angular.module("myDirectives", [])
 	                return digits;
 	            });
 	        }
-	    };
-	});
+        };
+	})
+	.directive('nksOnlyNumber', function () {
+            return {
+                restrict: 'EA',
+                require: 'ngModel',
+                link: function (scope, element, attrs, ngModel) {   
+                     scope.$watch(attrs.ngModel, function(newValue, oldValue) {
+                          var spiltArray = String(newValue).split("");
+
+                          if(attrs.allowNegative == "false") {
+                            if(spiltArray[0] == '-') {
+                              newValue = newValue.replace("-", "");
+                              ngModel.$setViewValue(newValue);
+                              ngModel.$render();
+                            }
+                          }
+
+                          if(attrs.allowDecimal == "false") {
+                              newValue = parseInt(newValue);
+                              ngModel.$setViewValue(newValue);
+                              ngModel.$render();
+                          }
+
+                          if(attrs.allowDecimal != "false") {
+                            if(attrs.decimalUpto) {
+                               var n = String(newValue).split(".");
+                               if(n[1]) {
+                                  var n2 = n[1].slice(0, attrs.decimalUpto);
+                                  newValue = [n[0], n2].join(".");
+                                  ngModel.$setViewValue(newValue);
+                                  ngModel.$render();
+                               }
+                            }
+                          }
+
+
+                          if (spiltArray.length === 0) return;
+                          if (spiltArray.length === 1 && (spiltArray[0] == '-' || spiltArray[0] === '.' )) return;
+                          if (spiltArray.length === 2 && newValue === '-.') return;
+
+                        /*Check it is number or not.*/
+                        if (isNaN(newValue)) {
+                          ngModel.$setViewValue(oldValue);
+                          ngModel.$render();
+                        }
+                    });
+                }
+            };
+        });
